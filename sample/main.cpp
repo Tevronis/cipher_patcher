588ddc1eb6fae2cfb76fd933d962327da2358858
Mike <mishaenz@yandex.ru>
2019-03-18 00:33:13 +0400

    Windows support was added

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,7 +1,10 @@
 #include <cstring>
 #include <cstdio>
-#include <unistd.h>
 #include <stdlib.h>
+
+
+#ifdef linux
+#include <unistd.h>
 #include <sys/mman.h>
 
 int change_page_permissions_of_address(void *addr) {
@@ -14,6 +17,17 @@ int change_page_permissions_of_address(void *addr) {
 
     return 0;
 }
+#elif _WIN32
+#include <windows.h>
+int change_page_permissions_of_address(void *addr) {
+    DWORD old;
+
+    if (VirtualProtect(addr, sizeof(addr), PAGE_EXECUTE_READWRITE, &old)) {
+        // printf("Writable\n");
+    }
+    return 0;
+}
+#endif
 
 int check_secret(char* secret) {
     if (strlen(secret) == 8 && secret[0] == 'M' && secret[1] == 'E')
@@ -21,35 +35,35 @@ int check_secret(char* secret) {
     return 0;
 }
 
-void decrypt(void *addr, int key) {
+void decrypt(void *addr, int key, int func_len) {
     if (change_page_permissions_of_address(addr)==-1) {
          printf("change page permission error!\n");
     }
 
     unsigned char *start_instruction = (unsigned char*)addr;
 
-    for (unsigned char *pos = start_instruction; pos < start_instruction + 70; ++pos) {
+    for (unsigned char *pos = start_instruction; pos < start_instruction + func_len; ++pos) {
+        // printf("%u ", (unsigned int)*pos);
         *pos ^= key;
-	// printf("%u ", (unsigned int)*pos);
     }
     printf("Decrypt done!\n");
 }
 
 int main(int argc, char *argv[]) {
     void *ff = (void*)check_secret;
-    if (argc < 3) {
-        printf("Ex: ./<prog_name> <password> <decrypt_key>\n");
+    if (argc < 4) {
+        printf("Ex: ./<prog_name> <password> <decrypt_key> <len_of_decrypted_function>\n");
         return -1;
     }
 
-    decrypt(ff, atoi(argv[2]));
+    decrypt(ff, atoi(argv[2]), atoi(argv[3]));
 
     if (argc > 1) {
         if (check_secret(argv[1])) {
             printf("password correct\n");
         }
         else {
-            printf("password incorect!\n");
+            printf("password incorrect!\n");
 	    }
     }
     return 0;
